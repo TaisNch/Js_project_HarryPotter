d896bf8e07188a0fcb0a18e0be154236beb85ef7
Irina864 <irina.peslyak.1864@gmail.com>
2024-04-17 19:39:48 +0300

    add new functions

--- js/form.js
+++ js/form.js
@@ -1,7 +1,34 @@
+class User {
+    constructor(
+        userName,
+        userMail,
+        userAge,
+        userSex,
+        userHouse,
+        quote,
+        speaker
+    ) {
+        this.userName = userName;
+        this.userMail = userMail;
+        this.userAge = userAge;
+        this.userSex = userSex;
+        this.userHouse = userHouse;
+        this.quote = quote;
+        this.speaker = speaker;
+    }
+}
+
 const form = document.forms.form;
 const inputs = document.querySelectorAll('input');
 const formAgreement = form.elements.userAgreement;
 const formButton = form.elements.submitButton;
+
+const nameData = form.elements.userName;
+const mailData = form.elements.userEmail;
+const ageData = form.elements.userAge;
+const male = form.elements.radioOne;
+const female = form.elements.radioTwo;
+const houseData = form.elements.house;
 
 form.addEventListener('submit', checkAllInputs);
 
@@ -23,23 +50,25 @@ for (let input of inputs) {
 function checkValidity(input) {
     const validity = input.validity;
     if (validity.valueMissing) {
-        createNewErrorMessage('This field is empty', input);
+        createNewElement('This field is empty', input, 'error');
     }
     if (validity.patternMismatch) {
-        createNewErrorMessage('Invalid data format', input);
+        createNewElement('Invalid data format', input, 'error');
     }
     if (validity.rangeOverflow) {
         const max = getAttributeValue(input, 'max');
-        createNewErrorMessage(
+        createNewElement(
             `The maximum value cannot be greater than ${max}`,
-            input
+            input,
+            'error'
         );
     }
     if (validity.rangeUnderflow) {
         const min = getAttributeValue(input, 'min');
-        createNewErrorMessage(
+        createNewElement(
             `The minimum value cannot be less than ${min}`,
-            input
+            input,
+            'error'
         );
     }
 }
@@ -54,22 +83,59 @@ function checkAllInputs(e) {
     for (let input of inputs) {
         checkValidity(input);
     }
-    if (
-        form.elements.radioOne.checked === false &&
-        form.elements.radioTwo.checked === false
-    ) {
-        createNewErrorMessage('Choose one', document.getElementById('sex'));
+    if (male.checked === false && female.checked === false) {
+        createNewElement('Choose one', document.getElementById('sex'), 'error');
     }
-    if (form.elements.house.value === '') {
-        createNewErrorMessage('Choose one', form.elements.house);
+    if (houseData.value === '') {
+        createNewElement('Choose one', houseData, 'error');
     }
     formAgreement.checked = false;
     formButton.disabled = !formAgreement.checked;
+    createUserCard();
 }
 
-function createNewErrorMessage(content, elem) {
+function createNewElement(content, elem, classNew) {
     const errorMessage = document.createElement('div');
-    errorMessage.classList.add('error');
+    errorMessage.classList.add(classNew);
     errorMessage.textContent = content;
     elem.after(errorMessage);
 }
+
+async function createUserCard() {
+    const url = 'https://api.portkey.uk/quote';
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('No connection to the server');
+    }
+    const data = await response.json();
+    const randomQuote = data.quote;
+    const randomSpeaker = data.speaker;
+    let sex;
+    if (male.checked) {
+        sex = male.value;
+    }
+    if (female.checked) {
+        sex = female.value;
+    }
+    let newUser = new User(
+        nameData.value,
+        mailData.value,
+        ageData.value,
+        sex,
+        houseData.value,
+        randomQuote,
+        randomSpeaker
+    );
+    console.log(newUser);
+    window.localStorage.setItem(mailData.value, newUser);
+    const userLocalInfo = JSON.parse(
+        window.localStorage.getItem(mailData.value)
+    );
+    const userCard = document.querySelector('.usercard');
+    userCard.textContent = `<h2 class="usercard__title">User card</h2>
+    <div class="usercard__box"></div>`;
+    const userBox = document.querySelector('.usercard__box');
+    createNewElement(userBox);
+}
+
+createUserCard();
