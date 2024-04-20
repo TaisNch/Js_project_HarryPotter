d542645f54c7eb9ca868165a48254b8ab791f17f
vikrugpro <vikrugpro@gmail.com>
2024-04-20 09:46:23 +0300

    added music

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
 const audio = document.getElementById('audio');
-document.getElementById('play').addEventListener('click', () =>
-{
-audio.play()
-})
+const playButton = document.getElementById('play');
+let playing = false;
+audio.preload = 'auto';
+audio.addEventListener('ended', function() {
+    playing = false;
+});
+playButton.addEventListener('click', playStop);
+
+function playStop() {
+    if( playing) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      playing = !playing;
+}
