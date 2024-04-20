8209f66c14f624c03d65ee038d0298cc314dbefb
TaisNch <tais.aliexpress@gmail.com>
2024-04-20 10:53:34 +0200

    game

--- js/play_cards.js
+++ js/play_cards.js
@@ -1,75 +1,67 @@
-const game = document.getElementById("game")
-const cardsCount = 4;
+const cards = document.querySelectorAll('.cards__item');
+let hasCardFlipped = false;
+let blockLocked = false;
+let firstCard, secondCard;
+
+const flipCard = e => {
+if (blockLocked) return;
+const trgt= e.target.parentElement;
+if (trgt === firstCard) return;
+trgt.classList.add('flip');
+
+if (!hasCardFlipped) {
+    hasCardFlipped = true;
+    firstCard = trgt;
+}
+else {
+    
+    hasCardFlipped = false;
+    secondCard = trgt;
+    //check for match
+checkForMatch()
+}
+};
 
 
-function startGame(game) {
-  const cardsNumberArray = []
-  let firstCard = null
-  let secondCard = null
 
-
-  // Создание массива чисел
-  for (let i = 1; i <= cardsCount; i++) {
-    cardsNumberArray.push(i, i)
-  }
-
-  // Перемешивание массива чисел
-  for (let i = 0; i < cardsNumberArray.length; i++) {
-    let randomIndex = Math.floor(Math.random() * cardsNumberArray.length)
-
-    let temp = cardsNumberArray[i]
-    cardsNumberArray[i] = cardsNumberArray[randomIndex]
-    cardsNumberArray[randomIndex] = temp
-  }
-
-  // Создание карточек
-  for (const cardNumber of cardsNumberArray) {
-    let card = document.createElement("div")
-    card.textContent = cardNumber
-    card.classList.add("card")
-
-    // Клик по карточке
-    card.addEventListener("click", function () {
-      if (card.classList.contains("open") || card.classList.contains("success")) {
-        return
-      }
-
-      if (firstCard !== null && secondCard !== null) {
-        firstCard.classList.remove("open")
-        secondCard.classList.remove("open")
-        firstCard = null
-        secondCard = null
-      }
-
-      card.classList.add("open")
-
-      if (firstCard === null) {
-        firstCard = card
-      } else {
-        secondCard = card
-      }
-
-      if (firstCard !== null && secondCard !== null) {
-        let firstCardNumber = firstCard.textContent
-        let secondCardNumber = secondCard.textContent
-
-        if (firstCardNumber === secondCardNumber) {
-          firstCard.classList.add("success")
-          secondCard.classList.add("success")
-        }
-      }
-      if (cardsNumberArray.length === document.querySelectorAll(".success").length) {
-        setTimeout(function () {
-          game.innerHTML = ""
-          alert("You winn! Start one more time")
-          startGame(game)
-        }, 400)
-      }
-
-    })
-
-    game.append(card)
-  }
+const checkForMatch = () => {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        // firstCard.removeEventListener('click',flipCard);
+        // secondCard.removeEventListener('click',flipCard);
+    }
+    else {
+        unflipCards();
+    //     blockLocked = true;
+    //    setTimeout(() => {
+    //     firstCard.classList.remove('flip');
+    //     secondCard.classList.remove('flip');
+    //    }, 1000)
+    //    blockLocked = false;
+       
+    }
 }
 
-startGame(game)
+const disableCards = () => {
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+}
+
+const unflipCards = () => {
+    blockLocked = true;
+    setTimeout(() => {
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+    }, 1000)
+    reset()
+}
+
+const reset = () => {
+    hasCardFlipped=hasCardFlipped=false;
+    firstCard=secondCard=null;
+}
+cards.forEach(card => {
+    card.addEventListener('click', flipCard);
+    const rndmInex = Math.floor(Math.random()*cards.length);
+    card.style.order = rndmInex;
+})
