3e4097a391c351cefb2c21246d8870036c58474e
Irina864 <irina.peslyak.1864@gmail.com>
2024-04-21 15:19:08 +0300

    add new forms functions

--- js/form.js
+++ js/form.js
@@ -60,6 +60,126 @@ const programUsers = [
         '',
         './assets/images/img_main/hufflepuff.png'
     ),
+    new User(
+        'Jin',
+        'jin.ger23@gmail.com',
+        22,
+        'female',
+        'Gryffindor',
+        '',
+        '',
+        './assets/images/img_main/gryffindor.png'
+    ),
+    new User(
+        'Yura',
+        'yura.sakamoto99@gmail.com',
+        24,
+        'female',
+        'Ravenclaw',
+        '',
+        '',
+        './assets/images/img_main/ravenclaw.png'
+    ),
+    new User(
+        'Grab',
+        'grab.hettler@gmail.com',
+        45,
+        'male',
+        'Slytherin',
+        '',
+        '',
+        './assets/images/img_main/slytherin.png'
+    ),
+    new User(
+        'Rony',
+        'rony2002@gmail.com',
+        22,
+        'male',
+        'Hufflepuff',
+        '',
+        '',
+        './assets/images/img_main/hufflepuff.png'
+    ),
+    new User(
+        'Ruswelt',
+        'ruswelt.iris45@gmail.com',
+        30,
+        'male',
+        'Gryffindor',
+        '',
+        '',
+        './assets/images/img_main/gryffindor.png'
+    ),
+    new User(
+        'Rory',
+        'robert567@gmail.com',
+        32,
+        'male',
+        'Ravenclaw',
+        '',
+        '',
+        './assets/images/img_main/ravenclaw.png'
+    ),
+    new User(
+        'Diana',
+        'diana6548@gmail.com',
+        35,
+        'female',
+        'Slytherin',
+        '',
+        '',
+        './assets/images/img_main/slytherin.png'
+    ),
+    new User(
+        'George',
+        'georgina.smith23@gmail.com',
+        35,
+        'Female',
+        'Hufflepuff',
+        '',
+        '',
+        './assets/images/img_main/hufflepuff.png'
+    ),
+    new User(
+        'Luna',
+        'luna123@gmail.com',
+        30,
+        'female',
+        'Gryffindor',
+        '',
+        '',
+        './assets/images/img_main/gryffindor.png'
+    ),
+    new User(
+        'Josie',
+        'josefina32@gmail.com',
+        32,
+        'female',
+        'Ravenclaw',
+        '',
+        '',
+        './assets/images/img_main/ravenclaw.png'
+    ),
+    new User(
+        'Ilion',
+        'ilion.grey@gmail.com',
+        32,
+        'male',
+        'Slytherin',
+        '',
+        '',
+        './assets/images/img_main/slytherin.png'
+    ),
+    new User(
+        'Serg',
+        'serg.harwey@gmail.com',
+        35,
+        'male',
+        'Hufflepuff',
+        '',
+        '',
+        './assets/images/img_main/hufflepuff.png'
+    ),
 ];
 const users = [];
 const form = document.forms.form;
@@ -77,20 +197,17 @@ const findFriendSelect = findFriendForm.elements.findHouse;
 const findFriendButton = findFriendForm.elements.findFormButton;
 const findFriendResult = document.querySelector('.findfriend__result');
 
-form.addEventListener('submit', checkAllInputs);
-document.forms.findfriendForm.addEventListener('submit', findFriendsForUser);
-formAgreement.addEventListener('change', () => {
-    formButton.disabled = !formAgreement.checked;
-    if (formAgreement.checked === true) {
-        formButton.removeAttribute(disabled);
-    }
-});
-
 for (let i = 0; i < localStorage.length; i++) {
     users.push(localStorage.key(i));
 }
 if (localStorage.length < 16) {
-    for (let i = users.length; i < 16; i++) {}
+    for (let i = localStorage.length; i < 16; i++) {
+        let randomIndex = Math.floor(Math.random() * 16);
+        window.localStorage.setItem(
+            programUsers[randomIndex].userMail,
+            JSON.stringify(programUsers[randomIndex])
+        );
+    }
 }
 
 for (let input of inputs) {
@@ -101,6 +218,16 @@ for (let input of inputs) {
         input.style.border = '';
     });
 }
+
+form.addEventListener('submit', checkAllInputs);
+document.forms.findfriendForm.addEventListener('submit', findFriendsForUser);
+formAgreement.addEventListener('change', () => {
+    formButton.disabled = !formAgreement.checked;
+    if (formAgreement.checked === true) {
+        formButton.removeAttribute(disabled);
+    }
+});
+
 function checkValidity(input) {
     const validity = input.validity;
     if (validity.valueMissing) {
